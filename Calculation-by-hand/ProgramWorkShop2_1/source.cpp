0b7ebf9fda40e48eef90a8a653aba2dfb93ec32e
Harmony82 <seiyaxyz0918@gmail.com>
2023-10-18 09:47:30 +0900

    引き算の機能を追加

--- Calculation-by-hand/ProgramWorkShop2_1/source.cpp
+++ Calculation-by-hand/ProgramWorkShop2_1/source.cpp
@@ -1,3 +1,4 @@
+//https://github.com/Harmony82/Calculation-by-hand
 #include <iostream>
 #include<ctime>
 #include<cstdlib>
@@ -5,35 +6,101 @@
 #include<format>
 using namespace std;
 
-const int MAX_INPUT_DIGITS = 50;
-const int MAX_CALCULATION_DIGITS = 50+1;//1桁上がりうる
-bool Input(int conversionInt[MAX_CALCULATION_DIGITS])
+const int MAX_CALCULATION_DIGITS = 51;//1桁上がりうる
+/// <summary>
+/// 
+/// </summary>
+/// <param name="input"></param>
+/// <param name="conversionInt"></param>
+/// <returns>有効な入力だったか</returns>
+bool Conversion(string input, int conversionInt[MAX_CALCULATION_DIGITS])
 {
-	string input;
-	cin >> input;
-
+	bool signexist = false;
+	bool isvalid = true;//有効な入力か
 	for (int i = 0; i < input.length(); i++)
 	{
-		conversionInt[input.length() - 1 - i] = input[i] - '0';
 		//\0を除いた桁数分から逆向きに入れていく
+		conversionInt[input.length() - 1 - i] = input[i] - '0';
 	}
-//-を検知する
+	for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
+	{
+		//符号のあとは0のみ。つまり符号の入力は入力の最初のみ
+		if (signexist == true && conversionInt[i] != 0) {
+			isvalid = false;
+		}
+
+		if (conversionInt[i] >= 0 &&
+			conversionInt[i] <= 9)
+		{
+		}
+		else if (conversionInt[i] == '-' - '0')
+		{
+			signexist = true;
+		}
+		else
+		{
+			isvalid = false;
+		}
+	}
+	if (conversionInt[MAX_CALCULATION_DIGITS - 2] != 0 || conversionInt[MAX_CALCULATION_DIGITS - 1] != 0)
+	{
+		isvalid = false;
+	}
+
+	if (isvalid == false)
+	{
+		cout << "無効な入力です。数字あるいは-、+を入力して下さい。桁数は"<<MAX_CALCULATION_DIGITS <<"までです。"<< "\n";
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
-void StartProcessing(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS])
+/// <summary>
+/// 
+/// </summary>
+/// <param name="conversionInt"></param>
+/// <param name="signcheck"></param>
+/// <returns>引き算かどうか</returns>
+bool Input(int conversionInt[MAX_CALCULATION_DIGITS], bool signcheck = false)
 {
+	string input;
+	do
+	{
+		for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
+		{
+			conversionInt[i] = 0;
+		}
+		cin >> input;
+	} while (!Conversion(input, conversionInt));
+
+	if (signcheck != true) { return false; }
+	for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
+	{
+		if (conversionInt[i] == '-' - '0')
+		{
+			conversionInt[i] = 0;
+			return true;
+		}
+	}
+	return false;
+}
+void StartProcessing(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS], bool& isSubtraction)
+{
+	cout << "数字を入力して計算ができる。\n対応している桁は" << MAX_CALCULATION_DIGITS - 2 << "桁まで。";
 	cout << "1つ目の数値を入力" << "\n" << ":";
 	Input(inputTotal);
 	cout << "2つ目の数値を入力" << "\n" << ":";
-	Input(inputNow);
+	isSubtraction = Input(inputNow, true);
 }
-void AddNumber(int inputNow[MAX_CALCULATION_DIGITS])
+void AddNumber(int inputNow[MAX_CALCULATION_DIGITS], bool& isSubtraction)
 {
 	cout << "数字を入力" << "\n" << ":";
-	Input(inputNow);
+	isSubtraction = Input(inputNow, true);
 }
-void Calculation(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS])
+void Addition(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS])
 {
-	int moveup = 0;
 	for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
 	{
 		if (inputTotal[i] + inputNow[i] > 9)//繰り上がりあり
@@ -47,6 +114,21 @@ void Calculation(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCUL
 		}
 	}
 }
+void Subtraction(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS])
+{
+	for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
+	{
+		if (inputTotal[i] - inputNow[i] < 0)//繰り下がりあり
+		{
+			inputTotal[i] = inputTotal[i] - inputNow[i] + 10;
+			inputTotal[i + 1]--;
+		}
+		else//繰り下がりなし
+		{
+			inputTotal[i] = inputTotal[i] - inputNow[i];
+		}
+	}
+}
 void DisplayNumber(int Numbers[MAX_CALCULATION_DIGITS])
 {
 	bool displayfrag = false;
@@ -56,9 +138,12 @@ void DisplayNumber(int Numbers[MAX_CALCULATION_DIGITS])
 			displayfrag = true;
 		if (displayfrag)
 			cout << Numbers[i];
+		else if (i == 0)
+			cout << 0;
 		else
 			cout << " ";
 	}
+
 }
 void DisplayFormula(int inputTotal[MAX_CALCULATION_DIGITS], int inputNow[MAX_CALCULATION_DIGITS])
 {
@@ -77,17 +162,32 @@ void DisplayAnswer(int inputTotal[MAX_CALCULATION_DIGITS])
 	DisplayNumber(inputTotal);
 	cout << "\n";
 }
+bool EndCheck(int Numbers[MAX_CALCULATION_DIGITS])
+{
+	for (int i = 0; i < MAX_CALCULATION_DIGITS; i++)
+	{
+		if (Numbers[i] != 0)
+			return false;
+	}
+	cout << "終了します";
+	return true;
+}
 int main()
 {
 	int inputTotal[MAX_CALCULATION_DIGITS] = { 0 };
 	int inputNow[MAX_CALCULATION_DIGITS] = { 0 };
+	bool isSubtraction;
 
-	StartProcessing(inputTotal, inputNow);
+	StartProcessing(inputTotal, inputNow, isSubtraction);
 	while (true)
 	{
 		DisplayFormula(inputTotal, inputNow);
-		Calculation(inputTotal, inputNow);
+		if (isSubtraction)
+			Subtraction(inputTotal, inputNow);
+		else
+			Addition(inputTotal, inputNow);
 		DisplayAnswer(inputTotal);
-		AddNumber(inputNow);
+		AddNumber(inputNow, isSubtraction);
+		if (EndCheck(inputNow))break;
 	}
 }
